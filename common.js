347db183234bc3b5c5d1adc3446318e3d211e33f
Robert Hohenester <robert.hohenester@googlemail.com>
2024-04-04 22:54:01 +0200

    basic softplay functions

---
+++ common.js
@@ -0,0 +1,14 @@
+// We use commonJS in node.js => using multiple Files within one project
+// Benefit: Structure the project, which is important
+
+// Therefore there are "modules". Each file has a particular responsiblity
+
+// e.g. building Spotify
+// Features:
+// Songs -> play, pause, skip forward, go backwards
+// Playlist -> create, edit, delete, reorder
+// Albums ...
+// Libraries ...
+// Friends
+// Shuffle
+// all features will have certain functions for what they need to be able to do => "scope of responsiblity"
--- src/soft-play.js
+++ src/soft-play.js
@@ -1,14 +1,48 @@
 // do not change these lines
-let adults = 0
-let children = 0
+const adults = 0
+const children = 0
 
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 // Start with the occupancy function.
+const numPeople = {
+  adults: adults,
+  children: children
+}
+
+function occupancy() {
+  return numPeople
+}
+
+function enter(numAdults, numChildren) {
+  if (numAdults < numChildren) {
+    return false
+  } else {
+    numPeople.adults += numAdults
+    numPeople.children += numChildren
+    return true
+  }
+}
+
+function leave(numAdults, numChildren) {
+  if (numAdults === 0) {
+    return false
+  } else if (numPeople.adults - numAdults < numPeople.children - numChildren) {
+    return false
+  } else if (numChildren > numAdults) {
+    return false
+  } else if (numPeople.adults < numAdults || numPeople.children < numChildren) {
+    return false
+  } else {
+    numPeople.adults -= numAdults
+    numPeople.children -= numChildren
+    return true
+  }
+}
 
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined
+  enter: enter,
+  leave: leave,
+  occupancy: occupancy
 }
